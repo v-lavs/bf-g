957e7f1b097be902316f496340379509e3052c54
v-lavs <lavskavanda@gmail.com>
2023-05-04 00:25:48 +0300

    add single-article.scss, single-news.html

--- src/js/main.js
+++ src/js/main.js
@@ -108,7 +108,7 @@ $(document).ready(function () {
 
     //SLIDERS
     let valuablesSlider;
-let advantagesSlider;
+    let advantagesSlider;
 
     function handleResponsive() {
 
@@ -185,6 +185,50 @@ let advantagesSlider;
         animation: 'fadeIn',
         overlayBackgroundColor: 'rgba (1,1,1, .25)'
     });
+// VIDEO YOUTUBE
+    // selector of all videos on the page
+    const videos = document.querySelectorAll('.video');
+
+// generate video url
+    let generateUrl = function(id) {
+        let query = '?rel=0&showinfo=0&autoplay=1';
+
+        return 'https://www.youtube.com/embed/' + id + query;
+    };
+
+// creating iframe
+    let createIframe = function(id) {
+        let iframe = document.createElement('iframe');
+
+        iframe.setAttribute('allowfullscreen', '');
+        iframe.setAttribute('allow', 'autoplay; encrypted-media');
+        iframe.setAttribute('src', generateUrl(id));
+
+        return iframe;
+    };
+
+// main code
+    videos.forEach((el) => {
+        let videoHref = el.getAttribute('data-video');
+
+        let deletedLength = 'https://youtu.be/'.length;
+
+        let videoId = videoHref.substring(deletedLength, videoHref.length);
+
+        let img = el.querySelector('img');
+        let youtubeImgSrc = 'https://i.ytimg.com/vi/' + videoId + '/maxresdefault.jpg';
+        img.setAttribute('src', youtubeImgSrc);
+
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            let iframe = createIframe(videoId);
+            el.querySelector('.video__poster').remove();
+            el.appendChild(iframe);
+            el.querySelector('.btn_play').remove();
+        });
+    });
+
 
 });
 
